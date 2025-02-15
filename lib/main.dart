7f1e06462755386e32692b6b1647f2689626a02b
giangthao <giangvtt@tasc.vn>
2025-02-15 13:09:51 +0700

    init project music app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/data/repository/repository.dart';
+
+void main() async {
+  var repository = DefaultRepository();
+  var songs = await repository.loadData();
+  if(songs != null && songs.isNotEmpty) {
+    for(var song in songs) {
+      debugPrint(song.toString());
+    }
+
+  }
+  else {
+    debugPrint("No songs");
+  }
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
